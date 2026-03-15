ae2cb64a2f738d32d7ea4fa4cf71cd2ac4d333bd
charafelidrissi1 <charafelidrissi1@gmail.com>
2026-03-15 17:23:37 +0000

    refactor: Update SQL query execution to use prepared statements with `getAsObject` and increase the position retrieval limit.

--- temp_check_db.js
+++ temp_check_db.js
@@ -1,9 +1,8 @@
-const SQL = require('sql.js');
+const initSqlJs = require('sql.js');
 const fs = require('fs');
 const path = require('path');
 
 async function check() {
-  const initSqlJs = require('sql.js');
   const SQL = await initSqlJs();
   const dbPath = '/root/trackpulse/tracking.db';
   const filebuffer = fs.readFileSync(dbPath);
@@ -12,13 +11,23 @@ async function check() {
   const imei = '350317170767788';
   console.log(`Checking data for IMEI: ${imei}`);
 
-  const device = db.exec(`SELECT id, name, last_update FROM devices WHERE imei = '${imei}'`);
-  console.log('Device info:', JSON.stringify(device, null, 2));
+  const deviceStmt = db.prepare(`SELECT id, name, last_seen, status FROM devices WHERE imei = '${imei}'`);
+  const deviceRows = [];
+  while (deviceStmt.step()) {
+    deviceRows.push(deviceStmt.getAsObject());
+  }
+  deviceStmt.free();
+  console.log('Device info:', JSON.stringify(deviceRows, null, 2));
 
-  if (device.length > 0 && device[0].values.length > 0) {
-    const deviceId = device[0].values[0][0];
-    const positions = db.exec(`SELECT latitude, longitude, altitude, speed, satellites, timestamp FROM positions WHERE device_id = ${deviceId} ORDER BY timestamp DESC LIMIT 5`);
-    console.log('Latest positions:', JSON.stringify(positions, null, 2));
+  if (deviceRows.length > 0) {
+    const deviceId = deviceRows[0].id;
+    const posStmt = db.prepare(`SELECT latitude, longitude, altitude, speed, satellites, timestamp FROM positions WHERE device_id = ${deviceId} ORDER BY timestamp DESC LIMIT 10`);
+    const posRows = [];
+    while (posStmt.step()) {
+      posRows.push(posStmt.getAsObject());
+    }
+    posStmt.free();
+    console.log('Latest positions:', JSON.stringify(posRows, null, 2));
   } else {
     console.log('Device not found in database.');
   }
